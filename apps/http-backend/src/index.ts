62e35a42b1d9ce9fbd232eccf9465ad26ebfcd45
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-11-04 12:52:36 +0530

    Initial http setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,14 @@
 import Express from "express";
+import jwt from "jsonwebtoken"
 
 const app = Express()
+
+app.post('/signup', (req, res) => {
+
+})
+
+app.post("/signin", (req, res) => {
+    
+})
 
 app.listen(3000, () => console.log("App listening on port 3000"))
