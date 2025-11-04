1212a186250175e79e3e096d28cc0a1f437168ef
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-11-04 11:42:15 +0530

    zod package

---
+++ packages/zod/src/types.ts
@@ -0,0 +1,17 @@
+import { z } from "zod"
+
+export const SignupSchema = z.object({
+    username: z.string(),
+    password: z.string(),
+    name: z.string()
+})
+
+export const SigninSchema = z.object({
+    username: z.string(),
+    password: z.string(),
+    name: z.string()
+})
+
+export const CreateRoomSchema = z.object({
+    name: z.string()
+})
