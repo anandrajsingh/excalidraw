9c370d9f502e36169fcc237608b829772f849ea6
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-11-06 12:42:37 +0530

    middleware

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -2,8 +2,24 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
-export function middleware (req: Request, res: Response, next: NextFunction){
-    const token = req.headers["authorization"] ?? ""
+export interface AuthRequest extends Request {
+    user?: any
+}
 
-    const decoded = jwt.verify(token, JWT_SECRET)
+export function middleware(req: AuthRequest, res: Response, next: NextFunction) {
+    const authHeader = req.headers["authorization"]
+
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        return res.status(401).json({ error: "No token provided" })
+    }
+
+    const token = authHeader.split(" ")[1] || "";
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(403).json({ error: "Invalid or expired token" });
+    }
 }
