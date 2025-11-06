65135a3c5ce369686537cd2abcbc140be96aa9f5
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-11-06 18:07:03 +0530

    signup signin route

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,72 @@
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { prismaClient } from "@repo/db/client";
+import { CreateRoomSchema, SigninSchema, SignupSchema } from "@repo/zod/types";
 import Express from "express";
 import jwt from "jsonwebtoken"
+import { AuthRequest, middleware } from "./middleware";
 
 const app = Express()
+app.use(Express.json())
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async(req, res) => {
+    const parsedData = SignupSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({message: "Incorrect Inputs."})
+        return
+    }
 
+    const user = await prismaClient.user.create({
+        data: {
+            email: parsedData.data?.email,
+            password: parsedData.data?.password,
+            name: parsedData.data?.name
+        }
+    })
+
+    res.json({userId: user.id})
 })
 
-app.post("/signin", (req, res) => {
-    
+app.post("/signin", async(req, res) => {
+    const parsedData = SigninSchema.safeParse(req.body)
+    if(!parsedData){
+        res.json({message: "Incorrect Input"})
+    }
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: parsedData.data?.email,
+            password: parsedData.data?.password
+        }
+    })
+
+    if(!user){
+        res.status(400).json({message: "Not Authorised."})
+    }
+
+    const token = jwt.sign({userId: user?.id}, JWT_SECRET)
+
+    res.json({token})
+})
+
+app.post("/room", middleware, async(req:AuthRequest, res) => {
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({mesage : "Incorrect Input"})
+        return;
+    }
+
+    try {
+        const room = await prismaClient.room.create({
+            data: {
+                slug: parsedData.data.name,
+                adminId: req.user.id
+            }
+        })
+
+        res.json({roomId: room.id})
+    } catch (error) {
+        res.status(400).json({message: "Room already exists with this name"})
+    }
 })
 
 app.listen(3000, () => console.log("App listening on port 3000"))
