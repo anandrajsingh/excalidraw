41b55b5d211e6faa7b7ffb7194ae6c82f1e79218
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-11-09 12:13:16 +0530

    ws

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,28 @@
-import {WebSocketServer} from "ws"
+import {WebSocket, WebSocketServer} from "ws"
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({port:8080});
+
+interface User {
+    ws: WebSocket,
+    rooms: string[],
+    userId: string
+}
+
+const users: User[] = []
+
+function checkUser (token:string): string | null {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(typeof decoded == "string"){
+        return null;
+    }
+    if(!decoded || !decoded.userId){
+        return null;
+    }
+    return decoded.userId;
+}
 
 wss.on("connection", function connection(ws, request){
     const url = request.url
@@ -10,12 +30,14 @@ wss.on("connection", function connection(ws, request){
 
     const queryParams = new URLSearchParams(url.split("?")[1]);
     const token = queryParams.get('token') || "";
-    const decoded = jwt.verify(token, JWT_SECRET);
-
-    if(!decoded || !(decoded as JwtPayload).userId){
-        ws.close();
-        return;
+    const userId = checkUser(token)
+    if(userId == null){
+        ws.close()
+        return null;
     }
+
+    users.push({userId, rooms: [], ws})
+    
     ws.on('message', function message(data){
         ws.send("pong")
     })
